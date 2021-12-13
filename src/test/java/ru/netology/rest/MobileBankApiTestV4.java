4b83b3f7a4df5b1845d30319a3565ce446c58a65
aa_maslov <smart.maslov@gmail.com>
2021-12-13 05:30:32 +0300

    remove

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -10,7 +10,8 @@ class MobileBankApiTestV4 {
     void shouldReturnDemoAccounts() {
 
         given()
-                .baseUri("http://localhost:9999/api/v1")
+                .baseUri("http://localhost:9999" +
+                        "")
                 .when()
                 .get("/demo/accounts")
                 .then()
